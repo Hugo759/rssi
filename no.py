b09b7885fc3294d63e9e658102ad0895c549a285
Hugo <44436512+Hugo759@users.noreply.github.com>
2018-12-06 12:26:35 -0500

    Add files via upload

---
+++ no.py
@@ -0,0 +1,14 @@
+raspberrypi (sec)
+mac:
+24:92:0E:29:AB:1D 
+35.32.126.121  /**/
+
+sudo btmon | grep RSSI & watch sudo hcitool scan
+
+sudo apt-get install libusb-dev libdbus-1-dev libglib2.0-dev
+sudo apt-get install libudev-dev libical-dev libreadline-dev
+sudo pip install bluepy
+
+http://www.switchdoc.com/2014/08/ibeacon-raspberry-pi-scanner-python/
+https://github.com/taka-wang/py-beacon
+https://github.com/jongillespie/LoNet_Edge-RPi
